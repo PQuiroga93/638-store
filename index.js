71139ac4afe3664db6cdb833f10d5a84df063a52
PQuiroga93 <pamelaviviana1993@gmail.com>
2026-09-19 00:31:01 -0300

    fest: registrar ruta de color

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const productosRoutes = require('./routes/productos.routes');
 const categoriasRoutes = require('./routes/categorias.routes');
+const colorRoutes = require('./routes/color.routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
 app.use('/productos', productosRoutes);
 app.use('/categorias', categoriasRoutes);
+app.use('/color', colorRoutes);
 
 app.use(errorHandler); // Siempre al final
 
